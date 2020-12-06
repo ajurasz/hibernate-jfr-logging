de251e4d135a64501396240e551b9c0adfd788aa
ajurasz <arkadiusz.jurasz@gmail.com>
2020-12-06 14:37:19 +0100

    commit jfr event

--- src/main/kotlin/io/ajurasz/QueryRecorder.kt
+++ src/main/kotlin/io/ajurasz/QueryRecorder.kt
@@ -1,5 +1,6 @@
 package io.ajurasz
 
+import jdk.jfr.*
 import net.ttddyy.dsproxy.ExecutionInfo
 import net.ttddyy.dsproxy.QueryInfo
 import net.ttddyy.dsproxy.listener.QueryExecutionListener
@@ -7,14 +8,18 @@ import net.ttddyy.dsproxy.listener.QueryExecutionListener
 class QueryRecorder : QueryExecutionListener by QueryExecutionListener.DEFAULT {
     override fun beforeQuery(execInfo: ExecutionInfo?, queryInfoList: MutableList<QueryInfo>) {
         resolveQuery(queryInfoList)
+                .forEach(::publishEvent)
     }
 
-    private fun resolveQuery(queryInfoList: MutableList<QueryInfo>) {
-        queryInfoList
+    private fun resolveQuery(queryInfoList: MutableList<QueryInfo>) = queryInfoList
             .flatMap { it.parametersList.map { params -> Pair(it.query, params) } }
-            .forEach {
-                val query = Query.from(it.first, it.second)
-                println(query.toSql())
-            }
-    }
+            .map { (query, params) -> Query.from(query, params) }
+
+    private fun publishEvent(query: Query) = QueryEvent(query.toSql()).commit()
 }
+
+@Name("io.ajurasz.QueryEvent")
+@Label("Query")
+@Category("Hibernate")
+@StackTrace(false)
+class QueryEvent(@Label("Sql") val sql: String): Event()
